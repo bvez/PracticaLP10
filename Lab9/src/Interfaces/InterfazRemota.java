4d54d97c529744b57e6a3cb735f6fa38469ce33e
alulab14 <alulab14@inf.pucp.edu.pe>
2018-06-06 21:39:14 -0500

    Primer commit

---
+++ Lab9/src/Interfaces/InterfazRemota.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaces;
+
+import java.util.ArrayList;
+import Modelo.Aeropuerto;
+/**
+ *
+ * @author alulab14
+ */
+public interface InterfazRemota extends java.rmi.Remote{
+    public void listarAeropuertos();
+    public void mostrarDatosAeropuertos(int id);
+}
---
+++ Lab9/src/LogicaNegocios/LogicaAeropuerto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package LogicaNegocios;
+import Interfaces.InterfazRemota;        
+import Modelo.Aeropuerto;
+import java.util.ArrayList;
+import AccesoDatos.AccesoDatosAeropuerto;
+/**
+ *
+ * @author alulab14
+ */
+public class LogicaAeropuerto extends java.rmi.server.UnicastRemoteObject implements InterfazRemota{
+    public void listarAeropuertos(){
+        
+    }
+    
+    public void mostrarDatosAeropuertos(int id){
+        
+    }
+}
